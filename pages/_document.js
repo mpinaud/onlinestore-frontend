566d0d93e70ac06a7faadd1b717de9de68672dd5
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-05 16:41:17 -0800

    create custom document with ServerStyleSheet for styled-components

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+// This custom document prevents the server side render to show an un-styled UI before the client side mounts it styled components
+//  * [Next.js](https://github.com/zeit/next.js/#custom-document)
+//  * [Styled Components](https://www.styled-components.com/docs/advanced#nextjs)
+
+export default class MyDocument extends Document {
+    static getInitialProps({ renderPage }) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
+    }
+
+    render() {
+        return (
+            <html>
+                <Head>{this.props.styleTags}</Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}

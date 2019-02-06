1decb812ab33d55e1b4a2b1b3ebc578b2513ee71
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-05 23:43:58 -0800

    implement getInitialProps from Next.js and pass to Page's children

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,38 @@
 import App, { Container } from 'next/app';
-import Page from '../components/Page';
+
 // Next.js uses the App component to initialize pages. You can override it and control the page initialization which allows you to do:
 //  * Persisting layout between page changes
 //  * Keeping state when navigating pages
 //  * Custom error handling using componentDidCatch
 //  * Inject additional data into pages(for example by processing GraphQL queries)
+import Page from '../components/Page';
+
 import {ApolloProvider} from 'react-apollo';
 import withData from '../lib/withData';
 
 class MyApp extends App {
+    // Explicitly get page values to add to our URL from Next.js' getInitialProps() life-cycle method. This is called before render on the server side.
+    static async getInitialProps({Component, ctx}) {
+        let pageProps = {};
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        pageProps.query = ctx.query;
+        return {pageProps};
+    }
+
     render() {
-        const { Component, apollo } = this.props;
+        const {Component, apollo, pageProps} = this.props;
         return (
             <Container>
+                {/* Connected to GraphQL Yoga */}
                 <ApolloProvider client={apollo}>
                     <Page>
-                        <Component/>
+                        <Component {...pageProps} />
                     </Page>
                 </ApolloProvider>
             </Container>
-        )
+        );
     }
 }
 

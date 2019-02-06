a7cd8ff39582c7e29bb40e085ce34885cbb79e55
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-05 23:24:41 -0800

    implement ApolloProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,26 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
-
 // Next.js uses the App component to initialize pages. You can override it and control the page initialization which allows you to do:
 //  * Persisting layout between page changes
 //  * Keeping state when navigating pages
 //  * Custom error handling using componentDidCatch
 //  * Inject additional data into pages(for example by processing GraphQL queries)
+import {ApolloProvider} from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
     render() {
-        const { Component } = this.props;
+        const { Component, apollo } = this.props;
         return (
             <Container>
-                <Page>
-                    <Component/>
-                </Page>
+                <ApolloProvider client={apollo}>
+                    <Page>
+                        <Component/>
+                    </Page>
+                </ApolloProvider>
             </Container>
         )
     }
 }
 
-export default MyApp;
-
+export default withData(MyApp);

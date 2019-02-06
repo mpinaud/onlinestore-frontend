13990a899be59e9bd03699af27c0daec447e9e49
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-06 00:54:55 -0800

    create, implement, stylize Item component and use formatMoney function

--- components/Items.js
+++ components/Items.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Item from './Item';
 
 const ALL_ITEMS_QUERY = gql`
     query ALL_ITEMS_QUERY {
@@ -18,13 +19,14 @@ const ALL_ITEMS_QUERY = gql`
 
 const Center = styled.div`
     text-align: center;
-`
+`;
+
 const ItemsList = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 60px;
     max-width: ${props => props.theme.maxWidth};
-`
+`;
 
 class Items extends Component {
     render() {
@@ -32,12 +34,15 @@ class Items extends Component {
             <Center>
                 <Query query={ALL_ITEMS_QUERY}>
                     {({data, error, loading}) => {
-                        if (loading) return <p>Loading...</p>
-                        if (error) return <p>Error: {error.message}</p>
-                        console.log(data);
-                        return <ItemsList>
-                            {data.items.map((item, i) => <p key={i}>{item.title}</p>)}
-                        </ItemsList>;
+                        if (loading) return <p>Loading...</p>;
+                        if (error) return <p>Error: {error.message}</p>;
+                        return (
+                            <ItemsList>
+                                {data.items.map(item => (
+                                    <Item key={item.id} item={item} />
+                                ))}
+                            </ItemsList>
+                        );
                     }}
                 </Query>
             </Center>

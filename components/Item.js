22998ca889b0d00feb0f2b46c4bee3b6d3e53907
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-07 20:23:20 -0800

    create DeleteItem CTA and mutation

--- components/Item.js
+++ components/Item.js
@@ -33,7 +33,7 @@ class Item extends Component {
                         <a>Edit ✏️</a>
                     </Link>
                     <button>Add to CArt</button>
-                    <DeleteItem>Delete This Item</DeleteItem>
+                    <DeleteItem id={item.id}>Delete This Item</DeleteItem>
                 </div>
             </ItemStyles>
         );

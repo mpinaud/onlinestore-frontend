58b5bca39a4a7f316c589121f099e452ca1baf51
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-09 15:57:07 -0800

    add skip, first and orderBy args to show 4 items per page

--- components/Items.js
+++ components/Items.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import {perPage} from '../config';
 import Item from './Item';
 import Pagination from './Pagination';
 
 const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+        items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
             id
             title
             price
@@ -34,7 +35,13 @@ class Items extends Component {
         return (
             <Center>
                 <Pagination page={this.props.page} />
-                    <Query query={ALL_ITEMS_QUERY}>
+                    <Query 
+                        query={ALL_ITEMS_QUERY}
+                        // fetchPolicy="network-only"
+                        variables={{
+                            skip: this.props.page * perPage - perPage,
+                        }}
+                    >
                         {({data, error, loading}) => {
                             if (loading) return <p>Loading...</p>;
                             if (error) return <p>Error: {error.message}</p>;

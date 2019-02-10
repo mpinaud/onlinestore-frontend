eecdef7b318f01651d3a68cf735beb87d0d64f4a
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-10 09:18:33 -0800

    create Signup with resolver and implement refetch query of current user

--- components/User.js
+++ components/User.js
@@ -1,27 +1,28 @@
-import {Query} from 'react-apollo';
+
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 
 const CURRENT_USER_QUERY = gql`
-    query {
-        me {
-            id
-            email
-            name
-            permissions
-        }
+  query {
+    me {
+      id
+      email
+      name
+      permissions
     }
+  }
 `;
 
 const User = props => (
     <Query {...props} query={CURRENT_USER_QUERY}>
-        {payload => props.children(payload)}
+        {payload => console.log(payload) || props.children(payload)}
     </Query>
 );
 
-User.PropTypes = {
+User.propTypes = {
     children: PropTypes.func.isRequired,
 };
 
 export default User;
-export {CURRENT_USER_QUERY};
+export { CURRENT_USER_QUERY };

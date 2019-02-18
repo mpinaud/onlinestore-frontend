f473788944fe4c02bf0bdf40f64a7ecaed06109e
Mikey Pinaud <michael.pinaud@modop.com>
2019-02-18 08:55:14 -0800

    add admin to Nav bar and create formatMoney test

--- components/Nav.js
+++ components/Nav.js
@@ -8,47 +8,51 @@ import Signout from './Signout';
 
 const Nav = () => (
     <User>
-        {({data: {me}}) => (
-            <NavStyles>
-                {me && <p>{me.name}</p>}
-                <Link href="/items">
-                    <a>Shop</a>
-                </Link>
-                {me && (
-                    <>
-                        <Link href="/sell">
-                            <a>Sell</a>
-                        </Link>
-                        <Link href="/orders">
-                            <a>Orders</a>
-                        </Link>
-                        <Link href="/me">
-                            <a>Account</a>
-                        </Link>
-                        <Signout />
-                        <Mutation mutation={TOGGLE_CART_MUTATION}>
-                            {toggleCart => (
-                                <button onClick={toggleCart}>
-                                    My Cart
-                                    <CartCount
-                                        count={me.cart.reduce(
-                                            (tally, cartItem) =>
-                                                tally + cartItem.quantity,
-                                            0
-                                        )}
-                                    />
-                                </button>
-                            )}
-                        </Mutation>
-                    </>
-                )}
-                {!me && (
-                    <Link href="/signup">
-                        <a>Sign In</a>
+        {({data: {me}}) => {
+            return (
+                <NavStyles>
+                    {me && <p>{me.name}</p>}
+                    <Link href="/items">
+                        <a>Shop</a>
                     </Link>
-                )}
-            </NavStyles>
-        )}
+                    {me && (
+                        <>
+                            <Link href="/sell">
+                                <a>Sell</a>
+                            </Link>
+                            <Link href="/orders">
+                                <a>Orders</a>
+                            </Link>
+                            {me.permissions.includes('ADMIN') && (
+                                <Link href="/permissions">
+                                    <a>Admin</a>
+                                </Link>
+                            )}
+                            <Signout />
+                            <Mutation mutation={TOGGLE_CART_MUTATION}>
+                                {toggleCart => (
+                                    <button onClick={toggleCart}>
+                                        My Cart
+                                        <CartCount
+                                            count={me.cart.reduce(
+                                                (tally, cartItem) =>
+                                                    tally + cartItem.quantity,
+                                                0
+                                            )}
+                                        />
+                                    </button>
+                                )}
+                            </Mutation>
+                        </>
+                    )}
+                    {!me && (
+                        <Link href="/signup">
+                            <a>Sign In</a>
+                        </Link>
+                    )}
+                </NavStyles>
+            )}
+        }
     </User>
 );
 
